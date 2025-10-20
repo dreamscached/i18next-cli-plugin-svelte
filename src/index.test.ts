c7c1c320865578ac40fd21017601c5239a463552
Herman S. <dreamscache.d@gmail.com>
2025-10-20 18:30:05 +0200

    yarn format

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,7 @@
+import { parse } from "svelte/compiler";
 import { describe, expect, it } from "vitest";
 
 import i18nextSveltePlugin from "./index.js";
-import { parse } from "svelte/compiler";
 
 describe("i18nextSveltePlugin", () => {
 	it.each([
@@ -55,39 +55,55 @@ describe("i18nextSveltePlugin", () => {
 			source: `<script module>export const foobar = "bar";</script>`,
 			expected: `export const foobar = "bar";`
 		},
-        {
-            name: "multiple statements in <script> with no semicolons",
-            source: `<script>console.log("Hello")\nconsole.log("World!")</script>`,
-            expected: `console.log("Hello")
+		{
+			name: "multiple statements in <script> with no semicolons",
+			source: `<script>console.log("Hello")\nconsole.log("World!")</script>`,
+			expected: `console.log("Hello")
 console.log("World!")`
-        },
-        {
-            name: "ASI-unsafe component with instance and module <script> tags",
-            source: `<script>
+		},
+		{
+			name: "ASI-unsafe component with instance and module <script> tags",
+			source: `<script>
   const data = [1, 2, 3]
 </script>
 <script context="module">
   [4, 5, 6].forEach(n => console.log(n))
 </script>`,
-            expected: `
+			expected: `
   const data = [1, 2, 3]
 
 ;
   [4, 5, 6].forEach(n => console.log(n))
 `
-        }
+		}
 	])("should extract valid ES code: $name", ({ source, expected }) => {
 		const extracted = i18nextSveltePlugin.onLoad(source, "test.svelte");
-        expect(() => parse(extracted)).not.toThrow();
+		expect(() => parse(extracted)).not.toThrow();
 		expect(extracted).toEqual(expected);
 	});
 
 	it.each([
-		{ path: "test.svelte", source: "<script>console.log('test')</script>", expected: "console.log('test')" },
-		{ path: "test.ts", source: "<foobar> invalid svelte/ts code", expected: "<foobar> invalid svelte/ts code" },
-		{ path: "test.svelte.ts", source: "<foobar> invalid svelte/ts code", expected: "<foobar> invalid svelte/ts code" },
-		{ path: "svelte.ts", source: "<foobar> invalid svelte/ts code", expected: "<foobar> invalid svelte/ts code" },
-	])("should skip non-Svelte files: $path", ({path, source, expected}) => {
+		{
+			path: "test.svelte",
+			source: "<script>console.log('test')</script>",
+			expected: "console.log('test')"
+		},
+		{
+			path: "test.ts",
+			source: "<foobar> invalid svelte/ts code",
+			expected: "<foobar> invalid svelte/ts code"
+		},
+		{
+			path: "test.svelte.ts",
+			source: "<foobar> invalid svelte/ts code",
+			expected: "<foobar> invalid svelte/ts code"
+		},
+		{
+			path: "svelte.ts",
+			source: "<foobar> invalid svelte/ts code",
+			expected: "<foobar> invalid svelte/ts code"
+		}
+	])("should skip non-Svelte files: $path", ({ path, source, expected }) => {
 		const extracted = i18nextSveltePlugin.onLoad(source, path);
 		expect(extracted).toEqual(expected);
 	});
